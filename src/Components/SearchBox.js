685b1b4b41e813f54cc809ae5015bf2f50541edb
VimuKale <vimurk111@gmail.com>
2021-10-24 16:01:05 +0530

    Adding Robofriends project to git

---
+++ src/Components/SearchBox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchBox = ({searchChange}) => {
+
+	return(
+
+		<div className="pa2">
+
+			<input 
+				className='pa3 ba b--blue bg-lightest-blue br-pill'
+				type='search' 
+				placeholder='Search Robots' 
+				onChange={searchChange}
+			/> 
+
+		</div>	
+
+		);
+
+}
+
+export default SearchBox;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Vimu Kale',
+    username: 'vimu_rk',
+    email: 'vimurk111@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Ajinkya Bhagat',
+    username: 'ajinkyabhagat8556',
+    email: 'ajinkyabhagat8556@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Prachi Dimote',
+    username: 'prachi_11',
+    email: 'prachidimote11@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
